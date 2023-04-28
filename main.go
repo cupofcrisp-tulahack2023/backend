8d8cec87a8987e590c8395e45003d4d07a655df0
forxz <fast35890@gmail.com>
2023-04-28 20:21:09 +0300

    basic-route

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello world")
-
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
+	router.Run(":8080")
 }
